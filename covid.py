c02006605aa62426927f68ab6c4c8e9734f530bd
DotMars <dotmars@users.noreply.github.com>
2020-03-29 15:32:03 -0400

    Formatting

--- covid.py
+++ covid.py
@@ -30,10 +30,10 @@ CELL_RADIUS = 5
 
 Active_cells = []  # An array to store {n = POPULATION} number of Cell objects
 
-covid_29 = disease.Disease(arg_name = "COVID-29")
+covid_29 = disease.Disease(arg_name="COVID-29")
 
 
-# PYGAME COLORSS
+# PYGAME COLORS
 CELL_COLOR = pygame.Color(15, 82, 186)
 BLACK = pygame.Color(0, 0, 0)
 RED = pygame.Color(255, 0, 0)
@@ -60,7 +60,7 @@ def check_cell_overlap(x, y, z, a):
     check_count += 1
     print("Check count :", check_count)
 
-    distance = sqrt( (x - z)*(x - z) + (y - a)*(y- a) )
+    distance = sqrt((x - z)*(x - z) + (y - a)*(y - a))
     if distance < CELL_RADIUS:
         print("Overlapping because distance : " + str(distance))
         return True
@@ -68,18 +68,17 @@ def check_cell_overlap(x, y, z, a):
         # print("Not overlapping because distance : " + str(distance))
         return False
 
+
 def initialize_cells_uniformly(number_of_regions=4):
     global Active_cells
     initial_cell_positions = []
     region_size = (WIN_WIDTH/(number_of_regions/2),
                    WIN_HEIGHT/(number_of_regions/2))
-
-
-        # Generating random non overlapping cells positions
+    # Generating random non overlapping cells positions
     Active_cells.append(Cell.Cell(random.randint(
         PADDING, WIN_WIDTH - PADDING), random.randint(PADDING, WIN_HEIGHT - PADDING), 0))
     for i in range(POPULATION):
-        Active_cells.append(Cell.Cell(id = i+1))
+        Active_cells.append(Cell.Cell(id=i+1))
 
 
 def initialize_cells():
@@ -88,24 +87,24 @@ def initialize_cells():
 
     for i in range(POPULATION):
         Active_cells.append(
-            Cell.Cell(id = i))
-    
+            Cell.Cell(id=i))
+
     Active_cells[0].set_state("I")
     Active_cells[0].infected_with(covid_29)
+
 
 def spread():
     for cell in Active_cells:
         if cell.is_infected():
             for other_cell in Active_cells:
                 # if cell != other_cell:
-                    distance = cell.distance(other_cell)
-                    if distance[0] <= cell.disease.get_radius() and distance[1] <= cell.disease.get_radius():
-                        if distance[0] > 5 or distance[1] > 5:
-                            # print("Infected distance : ", distance[0], " ", distance[1])
-                            # print("Infected position : ", cell.get_position(), end = " ")
-                            # print("Other cell position : ", other_cell.get_position())
-                            other_cell.infected_with(cell.disease)
-
+                distance = cell.distance(other_cell)
+                if distance[0] <= cell.disease.get_radius() and distance[1] <= cell.disease.get_radius():
+                    if distance[0] > 5 or distance[1] > 5:
+                        # print("Infected distance : ", distance[0], " ", distance[1])
+                        # print("Infected position : ", cell.get_position(), end = " ")
+                        # print("Other cell position : ", other_cell.get_position())
+                        other_cell.infected_with(cell.disease)
 
 
 def initialize_world(screen):
@@ -138,9 +137,6 @@ def main():
             # cell.set_trajectory(random.randint(1, 9))
             # cell.update_random_destination()
             cell.update_central_dest_random_dest()
-
-
-        
         spread()
 
         draw_cells(screen)
--- disease.py
+++ disease.py
@@ -1,6 +1,6 @@
 # from covid import INFECTION_RATE
 class Disease(object):
-    def __init__(self, arg_name = "UKNOWN DISEASE", infection_rate = .1, infection_radius = 15):
+    def __init__(self, arg_name="UKNOWN DISEASE", infection_rate=.1, infection_radius=15):
         super().__init__()
         self.name = arg_name
         self.infection_rate = infection_rate
@@ -8,18 +8,22 @@ class Disease(object):
         self.radius = 10
         print("New disease discovered !", self.name)
 
-        self.infection_radius = infection_radius   # Any cells withing this radius are susceptible to be infected
-        self.infection_rate = infection_rate    # The chances of catching the disease if within the infection radius
+        # Any cells withing this radius are susceptible to be infected
+        self.infection_radius = infection_radius
+        # The chances of catching the disease if within the infection radius
+        self.infection_rate = infection_rate
 
     def set_radius(self, radius):
         self.infection_radius = radius
-    
+
     def set_infection_rate(self, infection_rate):
         self.infection_rate = infection_rate
-    
+
     def get_name(self):
         return name
+
     def get_radius(self):
         return self.infection_radius
+
     def get_rate(self):
         return self.infection_rate
