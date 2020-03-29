6bd58439e34c6ffd4a8366ece79c20b808c76342
DotMars <dotmars@users.noreply.github.com>
2020-03-29 17:46:20 -0400

    Visualize matplot update

--- visualize.py
+++ visualize.py
@@ -1,41 +1,38 @@
-import matplotlib.pyplot as plt
-from matplotlib.collections import EventCollection
 import numpy as np
-import csv
+import matplotlib.pyplot as plt
+import pandas as pd
 
-with open('data.csv', newline='') as f:
-    reader = csv.reader(f)
-    data = list(reader)
+# with open('data.csv', newline='') as f:
+#     reader = csv.reader(f)
+#     data = list(reader)
 
+# infected_data = []
+# susceptible_data = []
+# remove_data = []
+# time_data = []
+# i = 0
+# for entry in data:
+#     infected_data.append(entry[0])
+#     susceptible_data.append(entry[1])
+#     remove_data.append(entry[2])
+#     time_data.append(i)
+#     i += 1
 
-infected_data = []
-susceptible_data = []
-remove_data = []
-time_data = []
-i = 0
-for entry in data:
-    infected_data.append(entry[0])
-    susceptible_data.append(entry[1])
-    remove_data.append(entry[2])
-    time_data.append(i)
-    i += 1
-    
+data = pd.read_csv("data.csv")
 
-# plot the data
-fig = plt.figure()
-ax1 = fig.add_subplot(111)
-# ax2 = fig.add_subplot(132)
-# ax3 = fig.add_subplot(133)
+x = data[0]
+y1 = data[1]
+y2 = data[2]
+y3 = data[3]
 
-ax1.set_title('Infected')
-# ax2.set_title('Susceptible')
-# ax3.set_title('Removed')
+y = np.vstack([y1, y2, y3])
 
-ax1.plot(time_data, infected_data, color='tab:red')
-ax1.plot(time_data, susceptible_data, color='tab:blue')
-ax1.plot(time_data, remove_data, color='tab:grey')
+labels = ["Infected ", "Susceptible", "Remove"]
 
-
-
-# display the plot
+fig, ax = plt.subplots()
+ax.stackplot(x, y1, y2, y3, labels=labels)
+ax.legend(loc='upper left')
 plt.show()
+
+
+
