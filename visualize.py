d863194464b61c7598ea11e6cbf55f7eba6513f7
DotMars <dotmars@users.noreply.github.com>
2020-04-01 21:12:05 -0400

    Fixed plotting problem

--- visualize.py
+++ visualize.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 import matplotlib.pyplot as plt
 import pandas as pd
 import csv
@@ -13,6 +14,7 @@ infected_data = []
 susceptible_data = []
 remove_data = []
 time_data = []
+
 i = 0
 for entry in data:
     infected_data.append(entry[0])
@@ -21,14 +23,12 @@ for entry in data:
     time_data.append(i)
     i += 1
 
-
-
-
 x = time_data
 y1 = infected_data
 y2 = susceptible_data
 y3 = remove_data
 
+np.set_printoptions(threshold = sys.maxsize)
 y = np.vstack([y1, y2, y3])
 
 # x = np.linspace(0, 10, 500)
@@ -41,10 +41,10 @@ fig, ax = plt.subplots()
 # plt.show()
 
 # Plotting
-line1, = ax.plot(x , dashes=[6, 2], label = labels[0])
-line2, = ax.plot(y1 , dashes=[6, 2], label = labels[1])
+# line1, = ax.plot(x , dashes=[6, 2], label = labels[0])
+# line2, = ax.plot(y1 , dashes=[6, 2], label = labels[1])
 line3, = ax.plot(y2 , dashes=[6, 2], label = labels[2])
-line4, = ax.plot(y3 , dashes=[6, 2], label = labels[3])
+# line4, = ax.plot(y3 , dashes=[6, 2], label = labels[3])
 
 ax.legend()
 plt.show()
