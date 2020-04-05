8964a027a591783ddef05905e7e795b10f7811dc
DotMars <fikriyass@users.noreply.github.com>
2020-04-05 15:26:04 -0400

    Formatting

--- visualize.py
+++ visualize.py
@@ -28,7 +28,7 @@ y1 = infected_data
 y2 = susceptible_data
 y3 = remove_data
 
-np.set_printoptions(threshold = sys.maxsize)
+np.set_printoptions(threshold=sys.maxsize)
 y = np.vstack([y1, y2, y3])
 
 # x = np.linspace(0, 10, 500)
@@ -43,8 +43,8 @@ fig, ax = plt.subplots()
 # Plotting
 # line1, = ax.plot(x , dashes=[6, 2], label = labels[0])
 # line2, = ax.plot(y1 , dashes=[6, 2], label = labels[1])
-line3, = ax.plot(y2 , dashes=[6, 2], label = labels[2])
-# line4, = ax.plot(y3 , dashes=[6, 2], label = labels[3])
+# line3, = ax.plot(y2 , dashes=[6, 2], label = labels[2])
+line4, = ax.plot(y3, dashes=[6, 2], label=labels[3])
 
 ax.legend()
 plt.show()
